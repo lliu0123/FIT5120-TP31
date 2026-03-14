85c7055d40e5dc12343553d1ce9292c205c550d3
JIAXUANWEI <jwei0062@student.moansh.edu>
2026-03-14 23:27:10 +1100

    vite config

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        awareness: resolve(__dirname, 'awareness.html'),
+        tools: resolve(__dirname, 'tools.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        calculator: resolve(__dirname, 'calculator.html'),
+        clothing_advice: resolve(__dirname, 'clothing_advice.html'),
+      },
+    },
+  },
 })
